423ef101760819496bd638fb6282ab3e9fe6468d
hillary-254 <hillarykim001@gmail.com>
2023-10-31 21:34:30 +0300

    fixing documentation issue

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Contains the FileStorage class"""
+"""
+Contains the FileStorage class
+"""
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
